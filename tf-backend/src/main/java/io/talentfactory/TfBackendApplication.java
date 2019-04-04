a8b8b60fffe280bbf5ff3a27596bd4a2afb54422
Unknown <arthur.delamare1@viacesi.fr>
2019-04-04 19:33:05 +0200

    init(back): spring boot initialization

---
+++ tf-backend/src/main/java/io/talentfactory/TfBackendApplication.java
@@ -0,0 +1,13 @@
+package io.talentfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfBackendApplication.class, args);
+	}
+
+}
---
+++ tf-backend/src/test/java/io/talentfactory/TfBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package io.talentfactory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TfBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
