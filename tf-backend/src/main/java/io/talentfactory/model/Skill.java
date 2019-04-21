c2ebc61a1d626db1db52e0e5c5769e29c82c50d0
Unknown <arthur.delamare1@viacesi.fr>
2019-04-21 23:58:08 +0200

    feat(skill): add skill model class

---
+++ tf-backend/src/main/java/io/talentfactory/model/Skill.java
@@ -0,0 +1,22 @@
+package io.talentfactory.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name ="skill")
+public class Skill {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+
+}
